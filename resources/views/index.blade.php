f8aafecf832c31206d1c83a7990337c163cdf659
ziampas <kristian.olausson@gmail.com>
2019-06-03 21:53:05 +0200

    design edits

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 
 <h1 class="title is-4">Welcome to Movie Database</h1>
-<img src="https://images-i.jpimedia.uk/imagefetch/c_fill,f_auto,h_559,q_auto:eco,w_830/https://inews.co.uk/wp-content/uploads/2018/12/lukaku.jpg" alt="" style="height: 300px;">
+<img src="https://www.thenational.ae/image/policy:1.866991:1559028848/image/jpeg.jpg?f=16x9&w=1200&$p$f$w=dfa40e8" alt="" style="height: 300px;">
 @endsection
