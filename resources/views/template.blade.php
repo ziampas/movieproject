d1fe1f9189cacce9b60a70fb2563cdeaae49234a
ziampas <kristian.olausson@gmail.com>
2019-06-10 13:02:21 +0200

    design changes on actor

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,13 @@
     <title>Movie Database</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+<style>
+
+.article {
+  margin-bottom: 1rem;
+}
+
+</style>
   </head>
   <body>
 
