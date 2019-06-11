630c2a078e147373ee893d78cc70b37ffdf85831
Zeena <zeena_aywaz@yahoo.com>
2019-06-11 01:45:30 -0700

    genre id name updated

--- database/migrations/2019_06_03_110025_create_genres_table.php
+++ database/migrations/2019_06_03_110025_create_genres_table.php
@@ -14,7 +14,7 @@ class CreateGenresTable extends Migration
     public function up()
     {
       Schema::create('genres', function (Blueprint $table) {
-          $table->increments('genreid');
+          $table->increments('id');
           $table->string('genrename');
       });
     }
